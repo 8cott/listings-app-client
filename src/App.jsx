3f2c4b9cf2abd93a9e8d5430a67df9be2226d75c
Scott <109820423+8cott@users.noreply.github.com>
2023-07-21 15:35:46 -0400

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './components/Index';
 import Show from './components/Show';
-import Create from './components/Create'
+import Create from './components/Create';
 import Edit from './components/Edit';
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar';
 import Signup from '../../client/src/components/Signup';
 import Login from '../../client/src/components/Login';
 import { AuthProvider } from './components/AuthContext';
@@ -13,31 +13,31 @@ import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
     <AuthProvider>
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Index />} />
-        <Route path='/listings/:id' element={<Show />} />
-        <Route path='/listings/create' element={<Create />} />
-        <Route path='/listings/:id/edit' element={<Edit />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-      <ToastContainer
-        position="bottom-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-    </Router>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/listings/create" element={<Create />} />
+          <Route path="/listings/:id/edit" element={<Edit />} />
+          <Route path="/listings/:id" element={<Show />} />
+          <Route path="/" element={<Index />} />
+        </Routes>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </BrowserRouter>
     </AuthProvider>
   );
-}
+};
 
 export default App;

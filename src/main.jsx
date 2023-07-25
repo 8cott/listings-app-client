1ef587ada7ad80080432338c739afb325cc0495b
Scott <109820423+8cott@users.noreply.github.com>
2023-07-24 21:22:18 -0400

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+console.log('VITE_API_BASE_URL:', import.meta.env.VITE_API_BASE_URL); // Log the VITE_API_BASE_URL
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
